965f5e9384e26399a3f8ecaf1484bb15b85aa486
Dmitri Novikov <dimanovi@hotmail.com>
2014-08-16 20:24:49 -0400

    modified JDialogs to work with DisplayController
    each JDialog should setVisible(true) and have DISPOSE_ON_CLOSE as default
    exit operation

--- src/JDialogue/GanttDisplay.java
+++ src/JDialogue/GanttDisplay.java
@@ -53,6 +53,7 @@ public class GanttDisplay extends JDialog {
 		chartPanel.setPreferredSize(new java.awt.Dimension(1080, 720));
 		setContentPane(chartPanel);
 		this.setBounds(20, 20, 1000, 800);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		
 	}
 	
