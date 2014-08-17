fa0f235be3484d00b929a946783bec462c3440ed
David Gurnsey <dave.gurnsey@gmail.com>
2014-08-17 13:22:55 -0400

    ready to pull

--- src/Analysis/EarnedValue.java
+++ src/Analysis/EarnedValue.java
@@ -12,13 +12,13 @@ import PModel.Project;
 public class EarnedValue {
 
 	private Project project;
-	private double  daysSinceStart;
-	
-	public EarnedValue(Project project, double daysSinceStart){
-		
-		this.project 		= project;
+	private double daysSinceStart;
+
+	public EarnedValue(Project project, double daysSinceStart) {
+
+		this.project = project;
 		this.daysSinceStart = daysSinceStart;
-		
+
 		calculateBudgetAtCompletion();
 		calculatePlannedValue();
 		calculateEarnedValue();
@@ -31,113 +31,119 @@ public class EarnedValue {
 		calculateSchedulePerformanceIndex();
 		calculateEstimateAtCompletion();
 		calculateEstimateToComplete();
-		
+
 	}
-	
-	private void calculateBudgetAtCompletion(){
-		
+
+	private void calculateBudgetAtCompletion() {
+
 		double bac = 0;
-		
-		for(Activity a : project.getActivityList()){
+
+		for (Activity a : project.getActivityList()) {
 			bac += a.getPlannedValue();
 		}
 
 		project.setBudgetAtCompletion(bac);
 	}
-	
-	private void calculatePlannedValue(){
-		
+
+	private void calculatePlannedValue() {
+
 		double scheduledValue = 0;
-		
-		for(Activity a : project.getActivityList()){
-			if(a.getEarliestFinish() <= daysSinceStart){
+		double percentScheduled;
+
+		for (Activity a : project.getActivityList()) {
+			if (a.getEarliestFinish() <= daysSinceStart) {
 				scheduledValue += a.getPlannedValue();
-			}
-			else if(a.getLatestStart() < daysSinceStart){
-				if(a.getDuration() > 0){
-					double percentScheduled = (daysSinceStart - a.getLatestStart())/a.getDuration();
+			} else if (a.getLatestStart() < daysSinceStart) {
+				if (a.getDuration() > 0) {
+					percentScheduled = (daysSinceStart - a.getLatestStart())
+							/ a.getDuration();
 					scheduledValue += percentScheduled * a.getPlannedValue();
 				}
 			}
 		}
-
 		project.setPlannedValue(scheduledValue);
-		
 	}
-	
-	private void calculateEarnedValue(){
-		
+
+	private void calculateEarnedValue() {
+
 		double ev = 0;
-		
-		for(Activity a : project.getActivityList()){
+
+		for (Activity a : project.getActivityList()) {
 			ev += (a.getPlannedValue() * a.getPercentComplete());
 		}
-		
+
 		project.setEarnedValue(ev);
-		
+
 	}
-	
-	private void calculatePercentScheduledForCompletion(){
-		
-		if(project.getBudgetAtCompletion() > 0){
-			double sfc = project.getPlannedValue()/project.getBudgetAtCompletion();
+
+	private void calculatePercentScheduledForCompletion() {
+
+		if (project.getBudgetAtCompletion() > 0) {
+			double sfc = project.getPlannedValue()
+					/ project.getBudgetAtCompletion();
 			project.setPercentScheduledForCompletion(sfc);
 		}
-		
+
 	}
-	
-	private void calculateActualCost(){
-		
+
+	private void calculateActualCost() {
+
 		double ac = 0;
-		
-		for(Activity a : project.getActivityList()){
-			if(a.getStatus()){
+
+		for (Activity a : project.getActivityList()) {
+			if (a.getStatus()) {
 				ac += a.getActualCost();
-			}
-			else{
-				if(a.getPercentComplete() > 0){
+			} else {
+				if (a.getPercentComplete() > 0) {
 					ac += a.getActualCost() * a.getPercentComplete();
 				}
 			}
 		}
-		
+
 		project.setActualCost(ac);
 	}
-	
-	private void calculatePercentComplete(){
-		if(project.getBudgetAtCompletion() > 0){
-			double pc = project.getEarnedValue()/project.getBudgetAtCompletion();
+
+	private void calculatePercentComplete() {
+		if (project.getBudgetAtCompletion() > 0) {
+			double pc = project.getEarnedValue()
+					/ project.getBudgetAtCompletion();
 			project.setPercentComplete(pc);
 		}
 	}
-	
-	private void calculateCostVariance(){
-		project.setCostVariance(project.getEarnedValue()-project.getActualCost());
+
+	private void calculateCostVariance() {
+		project.setCostVariance(project.getEarnedValue()
+				- project.getActualCost());
 	}
-	
-	private void calculateScheduleVariance(){
-		project.setScheduleVariance(project.getEarnedValue()-project.getPlannedValue());
+
+	private void calculateScheduleVariance() {
+		project.setScheduleVariance(project.getEarnedValue()
+				- project.getPlannedValue());
 	}
-	
-	private void calculateCostPerformanceIndex(){
-		if(project.getActualCost() > 0){
-			project.setCostPerformanceIndex(project.getEarnedValue()/project.getActualCost());
+
+	private void calculateCostPerformanceIndex() {
+		if (project.getActualCost() > 0) {
+			project.setCostPerformanceIndex(project.getEarnedValue()
+					/ project.getActualCost());
 		}
 	}
-	
-	private void calculateSchedulePerformanceIndex(){
-		if(project.getPlannedValue() > 0){
-			project.setSchedulePerformanceIndex(project.getEarnedValue()/project.getPlannedValue());
+
+	private void calculateSchedulePerformanceIndex() {
+		if (project.getPlannedValue() > 0) {
+			project.setSchedulePerformanceIndex(project.getEarnedValue()
+					/ project.getPlannedValue());
 		}
 	}
-	
-	private void calculateEstimateAtCompletion(){
-		if(project.getCostPerformanceIndex() > 0){
-			project.setEstimateAtCompletion(project.getBudgetAtCompletion()/project.getCostPerformanceIndex());
+
+	private void calculateEstimateAtCompletion() {
+		if (project.getCostPerformanceIndex() > 0) {
+			project.setEstimateAtCompletion(project.getBudgetAtCompletion()
+					/ project.getCostPerformanceIndex());
 		}
 	}
-	
-	private void calculateEstimateToComplete(){
-		project.setEstimateToComplete(project.getEstimateAtCompletion()-project.getActualCost());
+
+	private void calculateEstimateToComplete() {
+		project.setEstimateToComplete(project.getEstimateAtCompletion()
+				- project.getActualCost());
 	}
 }
