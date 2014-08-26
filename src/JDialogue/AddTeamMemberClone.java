b9f489aba47663a6ed237d3c0ef1badafeea784a
Dmitri Novikov <dimanovi@hotmail.com>
2014-08-25 20:34:16 -0400

    refactored AddTeamMemberClone

--- src/JDialogue/AddTeamMemberClone.java
+++ src/JDialogue/AddTeamMemberClone.java
@@ -1,124 +1,63 @@
 package JDialogue;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.Dialog.ModalityType;
-import java.awt.List;
-import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.Date;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.border.EmptyBorder;
 
-import Controller.MainController;
-import PModel.Activity;
-import PModel.Member;
-import PModel.MemberActivity;
-import PModel.Project;
-
-import com.toedter.calendar.JDateChooser;
-
+import Controller.DisplayController;
 import javax.swing.JComboBox;
-import javax.swing.JList;
 
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-
-/**
- * A dialogue used to create new activity of the project
- * 
- * @author Administrator
- *
- */
 public class AddTeamMemberClone extends JDialog {
 
-	private Activity activity;
-	private ArrayList<Member> teamMembers;
+	private Integer memberId;
 
-	public AddTeamMemberClone(Activity a) {
-		activity = a;
-		teamMembers = MainController.get()
-				.getMemberListForAddMemberToActivity();
-
+	public AddTeamMemberClone(final JComboBox<PModel.Member> memberComboBox) {
 		setModalityType(ModalityType.APPLICATION_MODAL);
 		setTitle("Add Team Member");
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		setLocationRelativeTo(null);
-		setBounds(100, 100, 477, 593);
-		setVisible(true);
-
-		getContentPane().setLayout(new BorderLayout());
-		getContentPane().setBackground(new Color(204, 255, 204));
-		//getContentPane().setBorder(new EmptyBorder(5, 5, 5, 5));
-		//getContentPane().add(getContentPane(), BorderLayout.CENTER);
+		setBounds(100, 100, 477, 253);
 		getContentPane().setLayout(null);
+		getContentPane().setBackground(new Color(204, 255, 204));
 
 		JLabel lblSelectTeamMember = new JLabel(
 				"Select Team Member to Add to Activity");
 		lblSelectTeamMember.setBounds(35, 24, 297, 22);
 		getContentPane().add(lblSelectTeamMember);
 
-		JLabel lblMembers = new JLabel("Members");
-		lblMembers.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-			}// /WHAT
-		});
-		lblMembers.setBounds(92, 207, 61, 16);
+		JLabel lblMembers = new JLabel("Members:");
+		lblMembers.setBounds(79, 84, 61, 16);
 		getContentPane().add(lblMembers);
 
-		final JComboBox membersComboBox = new JComboBox();
-		membersComboBox.setBounds(189, 203, 193, 27);
-
-		for (Member m : teamMembers) {
-			membersComboBox.addItem(m);
-		}
-		getContentPane().add(membersComboBox);
+		memberComboBox.setBounds(174, 82, 277, 29);
+		getContentPane().add(memberComboBox);
 
 		JButton btnAdd = new JButton("Add");
-		btnAdd.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				int v = ((Member) membersComboBox.getSelectedItem())
-						.getMemberID();
+		btnAdd.setBounds(174, 138, 117, 29);
+		getContentPane().add(btnAdd);
 
-				MemberActivity ma = new MemberActivity(
-						((Member) membersComboBox.getSelectedItem())
-								.getMemberID(), activity.getProjectID(),
-						activity.getNumber());
-				MainController.get().initializeMemberActivity(ma);
-				//TODO: initilize members through displaycontroller and not MainController
+		btnAdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				memberId = ((PModel.Member)memberComboBox.getSelectedItem()).getMemberID();
+				DisplayController.get().setMemberIdToAdd(memberId);
+				dispose();
 			}
 		});
-		btnAdd.setBounds(265, 366, 117, 29);
-		getContentPane().add(btnAdd);
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("OK");
 
-				okButton.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.setBounds(334, 138, 117, 29);
+		getContentPane().add(btnCancel);
 
-					}
-				});
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				DisplayController.get().setMemberIdToAdd(null);
+				dispose();
 			}
-		}
-	}
+		});
 
+		setVisible(true);
+	}
 }
